c3bc646f68375f36fa090434380be8ae58ad8cef
chiemelie <uzochukwuchiemelie@gmai>
2026-02-17 16:53:13 +0100

    GG

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,18 +1,25 @@
-import type {Response,NextFunction} from "express";
+import type { Response, NextFunction } from "express";
 import type { AuthRequest } from "../middleware/auth.js";
- import { User } from "../models/User.js";
+import { User } from "../models/User.js";
 
-export async function getUsers(req:AuthRequest, res:Response, next:NextFunction) {
-
-    try{
+export async function getUsers(req: AuthRequest, res: Response, next: NextFunction) {
+    try {
         const userId = req.userId;
+        const { search } = req.query;
 
-        const users = await User.find({ _id: { $ne: userId } }).select("name email avatar").limit(50);
+        let query: any = { _id: { $ne: userId } };
 
-res.json(users);
-    }catch (error) {
+        if (search) {
+            query.$or = [
+                { name: { $regex: search, $options: 'i' } },
+                { email: { $regex: search, $options: 'i' } }
+            ];
+        }
+
+        const users = await User.find(query).select("name email avatar").limit(50);
+        res.json(users);
+    } catch (error) {
         res.status(500);
         next(error);
-    }   
-       
     }
+}
